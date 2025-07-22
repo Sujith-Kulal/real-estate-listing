f4b13bd09e6088c30f22945d4bf2a5bcb4c08c76
Sujith-Kulal <sujithkulal909@gmail.com>
2025-07-22 19:45:40 +0530

    first commit of BHUMI

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About BHUMI</h1>
+      <p className='mb-4 text-slate-700'>BHUMI is a leading land-selling platform that specializes in helping clients buy, sell, and exchange land in the most desirable locations. Our team of experienced consultants is dedicated to providing exceptional service and making the land transaction process as smooth and transparent as possible.</p>
+      <p className='mb-4 text-slate-700'>BHUMI is a cutting-edge solution designed to transform the land marketplace by offering a seamless platform for managing land listings and connecting genuine landowners with serious buyers.</p>
+     </div>
+  )
+}
